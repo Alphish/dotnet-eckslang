3c16667f51237ce05e9e0a82318f8d42e5c94d11
Alice Jankowska <alphish.creature@gmail.com>
2024-12-31 20:51:16 +0100

    ISS-1: Added tests for the Eckslang cursor

---
+++ Alphicsh.Eckslang/Alphicsh.Eckslang.Tests/EckslangCursorTests.cs
@@ -0,0 +1,184 @@
+﻿using Alphicsh.Eckslang.Scanning;
+
+namespace Alphicsh.Eckslang.Tests;
+
+public class EckslangCursorTests
+{
+    [Fact]
+    public void ShouldReadSimpleString()
+    {
+        GivenTail("");
+        GivenSpan("Hello, world!");
+        GivenHead("");
+        WhenScannerMoved();
+        ThenInitialCursorShouldBe(position: 0, line: 1, column: 1);
+        ThenAdvanceCursorShouldBe(position: 13, line: 1, column: 14);
+        ThenBacktrackCursorShouldMatchInitialCursor();
+    }
+
+    [Fact]
+    public void ShouldHandleLfNewlines()
+    {
+        GivenTail("Lorem\nipsum ");
+        GivenSpan("dolor\nsit\namet,");
+        GivenHead(" consectetur\nadipiscing\nelit");
+        WhenScannerMoved();
+        ThenInitialCursorShouldBe(position: 12, line: 2, column: 7);
+        ThenAdvanceCursorShouldBe(position: 27, line: 4, column: 6);
+        ThenBacktrackCursorShouldMatchInitialCursor();
+    }
+
+    [Fact]
+    public void ShouldHandleCrNewlines()
+    {
+        GivenTail("Lorem\ripsum ");
+        GivenSpan("dolor\rsit\ramet,");
+        GivenHead(" consectetur\radipiscing\relit");
+        WhenScannerMoved();
+        ThenInitialCursorShouldBe(position: 12, line: 2, column: 7);
+        ThenAdvanceCursorShouldBe(position: 27, line: 4, column: 6);
+        ThenBacktrackCursorShouldMatchInitialCursor();
+    }
+
+    [Fact]
+    public void ShouldHandleCrlfNewlines()
+    {
+        GivenTail("Lorem\r\nipsum ");
+        GivenSpan("dolor\r\nsit\r\namet,");
+        GivenHead(" consectetur\r\nadipiscing\r\nelit");
+        WhenScannerMoved();
+        ThenInitialCursorShouldBe(position: 13, line: 2, column: 7);
+        ThenAdvanceCursorShouldBe(position: 30, line: 4, column: 6);
+        ThenBacktrackCursorShouldMatchInitialCursor();
+    }
+
+    [Fact]
+    public void ShouldDoubleLfcrNewlines()
+    {
+        GivenTail("Lorem\n\ripsum ");
+        GivenSpan("dolor\n\rsit\n\ramet,");
+        GivenHead(" consectetur\n\radipiscing\n\relit");
+        WhenScannerMoved();
+        ThenInitialCursorShouldBe(position: 13, line: 3, column: 7);
+        ThenAdvanceCursorShouldBe(position: 30, line: 7, column: 6);
+        ThenBacktrackCursorShouldMatchInitialCursor();
+    }
+
+    [Fact]
+    public void ShouldPauseMidCrlfAdvance()
+    {
+        GivenTail("Lorem ipsum ");
+        GivenSpan("dolor sit amet,\r");
+        GivenHead("\n consectetur adipiscing elit");
+        WhenScannerMoved();
+        ThenInitialCursorShouldBe(position: 12, line: 1, column: 13);
+        ThenAdvanceCursorShouldBe(position: 28, line: 1, column: 29);
+        ThenBacktrackCursorShouldMatchInitialCursor();
+    }
+
+    [Fact]
+    public void ShouldAddLinePastCrWithoutLfAdvance()
+    {
+        GivenTail("Lorem ipsum ");
+        GivenSpan("dolor sit amet,\r");
+        GivenHead(" consectetur adipiscing elit");
+        WhenScannerMoved();
+        ThenInitialCursorShouldBe(position: 12, line: 1, column: 13);
+        ThenAdvanceCursorShouldBe(position: 28, line: 2, column: 1);
+        ThenBacktrackCursorShouldMatchInitialCursor();
+    }
+
+    [Fact]
+    public void ShouldAddLinePastLastCrAdvance()
+    {
+        GivenTail("Lorem ipsum ");
+        GivenSpan("dolor sit amet,\r");
+        GivenHead("");
+        WhenScannerMoved();
+        ThenInitialCursorShouldBe(position: 12, line: 1, column: 13);
+        ThenAdvanceCursorShouldBe(position: 28, line: 2, column: 1);
+        ThenBacktrackCursorShouldMatchInitialCursor();
+    }
+
+    [Fact]
+    public void ShouldPauseMidCrlfBacktrack()
+    {
+        GivenTail("Lorem ipsum\r");
+        GivenSpan("\ndolor sit amet,");
+        GivenHead(" consectetur adipiscing elit");
+        WhenScannerMoved();
+        ThenInitialCursorShouldBe(position: 12, line: 1, column: 13);
+        ThenAdvanceCursorShouldBe(position: 28, line: 2, column: 16);
+        ThenBacktrackCursorShouldMatchInitialCursor();
+    }
+
+    [Fact]
+    public void ShouldSubstractLinePastCrWithoutLfBacktrack()
+    {
+        GivenTail("Lorem ipsum ");
+        GivenSpan("\ndolor sit amet,");
+        GivenHead(" consectetur adipiscing elit");
+        WhenScannerMoved();
+        ThenInitialCursorShouldBe(position: 12, line: 1, column: 13);
+        ThenAdvanceCursorShouldBe(position: 28, line: 2, column: 16);
+        ThenBacktrackCursorShouldMatchInitialCursor();
+    }
+
+    [Fact]
+    public void ShouldSubtractLinePastLastCrBacktrack()
+    {
+        GivenTail("");
+        GivenSpan("\ndolor sit amet,");
+        GivenHead(" consectetur adipiscing elit");
+        WhenScannerMoved();
+        ThenInitialCursorShouldBe(position: 0, line: 1, column: 1);
+        ThenAdvanceCursorShouldBe(position: 16, line: 2, column: 16);
+        ThenBacktrackCursorShouldMatchInitialCursor();
+    }
+
+    // -------
+    // Helpers
+    // -------
+
+    private string Tail { get; set; } = default!;
+    private string Span { get; set; } = default!;
+    private string Head { get; set; } = default!;
+
+    private IEckslangCursor InitialCursor { get; set; } = default!;
+    private IEckslangCursor AdvanceCursor { get; set; } = default!;
+    private IEckslangCursor BacktrackCursor { get; set; } = default!;
+
+    private void GivenTail(string tail) => Tail = tail;
+    private void GivenSpan(string span) => Span = span;
+    private void GivenHead(string head) => Head = head;
+
+    private void WhenScannerMoved()
+    {
+        var scanner = new EckslangScanner(Tail + Span + Head);
+
+        scanner.JumpTo(Tail.Length);
+        InitialCursor = scanner.Cursor.Clone();
+
+        scanner.JumpTo(Tail.Length + Span.Length);
+        AdvanceCursor = scanner.Cursor.Clone();
+
+        scanner.JumpTo(Tail.Length);
+        BacktrackCursor = scanner.Cursor.Clone();
+    }
+
+    private void ThenInitialCursorShouldBe(int position, int line, int column)
+        => CheckCursor(InitialCursor, position, line, column);
+
+    private void ThenAdvanceCursorShouldBe(int position, int line, int column)
+        => CheckCursor(AdvanceCursor, position, line, column);
+
+    private void ThenBacktrackCursorShouldMatchInitialCursor()
+        => CheckCursor(BacktrackCursor, InitialCursor.Position, InitialCursor.Line, InitialCursor.Column);
+
+    private void CheckCursor(IEckslangCursor cursor, int position, int line, int column)
+    {
+        Assert.Equal(position, cursor.Position);
+        Assert.Equal(line, cursor.Line);
+        Assert.Equal(column, cursor.Column);
+    }
+}
