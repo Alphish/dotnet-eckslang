7014eb4768880eb64bfef414d0fa1928662bca0b
Alice Jankowska <alphish.creature@gmail.com>
2025-01-02 00:42:12 +0100

    ISS-1: Implemented a string-matching pattern

---
+++ Alphicsh.Eckslang/Alphicsh.Eckslang/Scanning/EckslangStringPattern.cs
@@ -0,0 +1,24 @@
+﻿namespace Alphicsh.Eckslang.Scanning;
+
+public class EckslangStringPattern : IEckslangPattern
+{
+    private string String { get; }
+    private StringComparison Comparison { get; }
+
+    public EckslangStringPattern(string str, StringComparison comparison)
+    {
+        String = str;
+        Comparison = comparison;
+    }
+
+    public EckslangStringPattern(string str) : this(str, StringComparison.Ordinal)
+    {
+    }
+
+    public ReadOnlySpan<char> Match(IEckslangScanner scanner)
+    {
+        return scanner.Head.StartsWith(String, Comparison)
+            ? scanner.Head.Slice(0, String.Length)
+            : ReadOnlySpan<char>.Empty;
+    }
+}
