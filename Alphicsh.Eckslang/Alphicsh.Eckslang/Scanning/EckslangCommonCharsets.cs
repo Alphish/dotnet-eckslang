17508de3b2e7bc259cb5b1d976981c2ef30a691d
Alice Jankowska <alphish.creature@gmail.com>
2025-01-25 00:28:27 +0100

    ISS-1: Defined some common charsets

---
+++ Alphicsh.Eckslang/Alphicsh.Eckslang/Scanning/EckslangCommonCharsets.cs
@@ -0,0 +1,9 @@
+﻿namespace Alphicsh.Eckslang.Scanning;
+
+public static class EckslangCommonCharsets
+{
+    public static IEckslangCharset AsciiWord { get; } = EckslangArrayCharset.FromCharacterClass("[_0-9A-Za-z]");
+    public static IEckslangCharset AsciiLetterUnderscore { get; } = EckslangArrayCharset.FromCharacterClass("[_A-Za-z]");
+    public static IEckslangCharset AsciiDigit { get; } = EckslangArrayCharset.FromCharacterClass("[0-9]");
+    public static IEckslangCharset AsciiSpace { get; } = EckslangArrayCharset.FromCharacterClass(@"\s", 128).Fill();
+}
