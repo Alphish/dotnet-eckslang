bf74acca1ef13a20b0425656e69bef85c2fa612f
Alice Jankowska <alphish.creature@gmail.com>
2025-02-01 11:11:32 +0100

    ISS-1: Created automated tests for scanning different text entities

---
+++ Alphicsh.Eckslang/Alphicsh.Eckslang.Tests/Scanning/ScanCharTests.cs
@@ -0,0 +1,95 @@
+﻿namespace Alphicsh.Eckslang.Tests.Scanning;
+
+public class ScanCharTests : BaseEckslangScannerTest
+{
+    private static TheoryDataRow<string, int, char, string> CreateCase(string description, string content, int startPosition, char character, string expectedString)
+        => new TheoryDataRow<string, int, char, string>(content, startPosition, character, expectedString).WithTestDisplayName(description);
+
+    public static IEnumerable<ITheoryDataRow> TestCases = new List<ITheoryDataRow>
+    {
+        CreateCase("ShouldHandleEmptyString", "", 0, 'l', ""),
+
+        CreateCase("ShouldScanCorrectContentStart", "lorem", 0, 'l', "l"),
+        CreateCase("ShouldHaltInvalidContentStart", "lorem", 0, 'o', ""),
+        CreateCase("ShouldScanCorrectContentMiddle", "lorem", 2, 'r', "r"),
+        CreateCase("ShouldHaltInvalidContentMiddle", "lorem", 2, 'e', ""),
+        CreateCase("ShouldScanCorrectContentEnd", "lorem", 4, 'm', "m"),
+        CreateCase("ShouldHaltInvalidContentEnd", "lorem", 4, 'e', ""),
+        CreateCase("ShouldHaltPastContentEnd", "lorem", 5, 'm', ""),
+    };
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldCheckHasAheadCorrectly(string content, int startPosition, char character, string expectedString)
+    {
+        GivenCharacter(character);
+        TestHasAhead(content, startPosition, expectedString);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldTrySkipCorrectly(string content, int startPosition, char character, string expectedString)
+    {
+        GivenCharacter(character);
+        TestTrySkip(content, startPosition, expectedString);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldExpectCorrectly(string content, int startPosition, char character, string expectedString)
+    {
+        GivenCharacter(character);
+        TestExpect(content, startPosition, expectedString);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldPeekCorrectly(string content, int startPosition, char character, string expectedString)
+    {
+        GivenCharacter(character);
+        TestPeek(content, startPosition, expectedString);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldTryReadCorrectly(string content, int startPosition, char character, string expectedString)
+    {
+        GivenCharacter(character);
+        TestTryRead(content, startPosition, expectedString);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldReadCorrectly(string content, int startPosition, char character, string expectedString)
+    {
+        GivenCharacter(character);
+        TestRead(content, startPosition, expectedString);
+    }
+
+    // -------
+    // Helpers
+    // -------
+
+    private char Character { get; set; }
+
+    private void GivenCharacter(char character)
+        => Character = character;
+
+    protected override void WhenCheckedAhead()
+        => CheckResult = Scanner.HasAhead(Character);
+
+    protected override void WhenSkipAttempted()
+        => CheckResult = Scanner.TrySkip(Character);
+
+    protected override void WhenSkipExpected()
+        => Scanner.Expect(Character);
+
+    protected override void WhenPeeked()
+        => ReadString = Scanner.Peek(Character).ToString();
+
+    protected override void WhenReadAttempted()
+        => ReadString = Scanner.TryRead(Character).ToString();
+
+    protected override void WhenReadExpected()
+        => ReadString = Scanner.Read(Character).ToString();
+}
---
+++ Alphicsh.Eckslang/Alphicsh.Eckslang.Tests/Scanning/ScanStringDefaultComparisonTests.cs
@@ -0,0 +1,98 @@
+﻿namespace Alphicsh.Eckslang.Tests.Scanning;
+
+public class ScanStringDefaultComparisonTests : BaseEckslangScannerTest
+{
+    private static TheoryDataRow<string, int, string, string> CreateCase(string description, string content, int startPosition, string inputString, string expectedString)
+        => new TheoryDataRow<string, int, string, string>(content, startPosition, inputString, expectedString).WithTestDisplayName(description);
+
+    public static IEnumerable<ITheoryDataRow> TestCases = new List<ITheoryDataRow>
+    {
+        CreateCase("ShouldHandleEmptyString", "", 0, "add", ""),
+
+        CreateCase("ShouldScanCorrectContentStart", "add element", 0, "add", "add"),
+        CreateCase("ShouldHaltInvalidContentStart", "remove element", 0, "add", ""),
+        CreateCase("ShouldScanCorrectContentMiddle", "common adder", 7, "add", "add"),
+        CreateCase("ShouldHaltInvalidContentMiddle", "common viper", 7, "add", ""),
+        CreateCase("ShouldScanCorrectContentEnd", "do add", 3, "add", "add"),
+        CreateCase("ShouldHaltInvalidContentEnd", "do remove", 6, "add", ""),
+        CreateCase("ShouldHaltPartiallyPastContentEnd", "nomad", 3, "add", ""),
+        CreateCase("ShouldHaltFullyPastContentEnd", "nomad", 5, "add", ""),
+
+        CreateCase("ShouldHaltInvalidContentCase", "COMMON ADDER", 7, "add", ""),
+    };
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldCheckHasAheadCorrectly(string content, int startPosition, string inputString, string expectedString)
+    {
+        GivenInputString(inputString);
+        TestHasAhead(content, startPosition, expectedString);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldTrySkipCorrectly(string content, int startPosition, string inputString, string expectedString)
+    {
+        GivenInputString(inputString);
+        TestTrySkip(content, startPosition, expectedString);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldExpectCorrectly(string content, int startPosition, string inputString, string expectedString)
+    {
+        GivenInputString(inputString);
+        TestExpect(content, startPosition, expectedString);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldPeekCorrectly(string content, int startPosition, string inputString, string expectedString)
+    {
+        GivenInputString(inputString);
+        TestPeek(content, startPosition, expectedString);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldTryReadCorrectly(string content, int startPosition, string inputString, string expectedString)
+    {
+        GivenInputString(inputString);
+        TestTryRead(content, startPosition, expectedString);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void ShouldReadCorrectly(string content, int startPosition, string inputString, string expectedString)
+    {
+        GivenInputString(inputString);
+        TestRead(content, startPosition, expectedString);
+    }
+
+    // -------
+    // Helpers
+    // -------
+
+    private string InputString { get; set; } = default!;
+
+    private void GivenInputString(string inputString)
+        => InputString = inputString;
+
+    protected override void WhenCheckedAhead()
+        => CheckResult = Scanner.HasAhead(InputString);
+
+    protected override void WhenSkipAttempted()
+        => CheckResult = Scanner.TrySkip(InputString);
+
+    protected override void WhenSkipExpected()
+        => Scanner.Expect(InputString);
+
+    protected override void WhenPeeked()
+        => ReadString = Scanner.Peek(InputString).ToString();
+
+    protected override void WhenReadAttempted()
+        => ReadString = Scanner.TryRead(InputString).ToString();
+
+    protected override void WhenReadExpected()
+        => ReadString = Scanner.Read(InputString).ToString();
+}
